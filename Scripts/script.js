a41364e3b1895f79775e0c8256fcc3140eec537c
sofyahoward <sofya@vt.edu>
2018-05-18 15:47:31 -0400

    changed the contact form and mobile layout

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,36 @@
+// redirect if on mobile
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   window.location = "https://sofyahoward.github.io/portfolio-m"; 
 }
+
+// Contact form JS
+window.onload = function(){ 
+  // Get the modal
+var contactForm = document.getElementById('formContainer');
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactFormBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+formContainer.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    contactForm.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+ if (event.target == formContainer) {
+   formContainer.style.display = "none";
+ }
+}
+};
 
 //Check to see if the window is top if not then display button
 $(window).scroll(function() {
@@ -36,3 +66,6 @@ $(document).ready(function(){
       hoverEnabled: false
     });
   });
+
+
+ 
